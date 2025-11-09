bf34e52d4b1772862fecabdb3339432ff5d0ab2e
josereis0 <josereismotta28@gmail.com>
2025-11-09 09:07:54 -0300

    Refactor Livro to extend Publicacao class
    Refactor Livro class to extend Publicacao and use its constructor.

--- SabrinaTestes/src/model/Livro.java
+++ SabrinaTestes/src/model/Livro.java
@@ -3,39 +3,17 @@ package model;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-public class Livro {
-    private String titulo;
+public class Livro extends Publicacao {
     private String autor;
-    private double preco;
-    private int estoque;
-    private int ano;
 
     public Livro(String titulo, String autor, double preco, int estoque, int ano) {
-        this.titulo = titulo;
+        super(titulo, preco, estoque, ano); // chama o construtor da superclasse
         this.autor = autor;
-        this.preco = preco;
-        this.estoque = estoque;
-        this.ano = ano;
     }
 
-    public String getTitulo() { return titulo; }
     public String getAutor() { return autor; }
-    public double getPreco() { return preco; }
-    public int getEstoque() { return estoque; }
-    public int getAno() { return ano; }
 
-    public boolean isDisponivel() {
-        return estoque > 0;
-    }
-
-    public void decrementarEstoque() {
-        if (estoque > 0) estoque--;
-    }
-
-    public void incrementarEstoque() {
-        estoque++;
-    }
-
+    @Override
     public void exibirInformacoes() {
         NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
         System.out.println("Título: " + titulo);
